2668ec0b386499a700ad448ba1e51ca43c8b542f
teolhyn <teemu.hynna@gmail.com>
2025-01-03 10:36:09 +0200

    feat: add header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,17 +1,32 @@
-import Avatar from "@mui/material/Avatar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Avatar
-          alt="Teemu Hynnä"
-          src="/static/images/avatar/1.jpg"
-          sx={{ width: 56, height: 56 }}
-        />
-        <p>Teemu Hynnä</p>
-        <p>Full Stack Developer</p>
+    <div className="App sticky top-0 backdrop-blur-lg z-10 flex justify-between items-center w-full font-custom">
+      <header className="App-header absolute left-1/2 transform -translate-x-1/2">
+        <div className="outline outline-1 rounded-full center m-5 py-5 px-8 flex justify-between text-sm w-96">
+          <a href="/" className="hover:text-teal-200 transition-colors duration-300">Home</a>
+          <a href="" className="hover:text-teal-200 transition-colors duration-300">About</a>
+          <a href="" className="hover:text-teal-200 transition-colors duration-300">Projects</a>
+          <a href="" className="hover:text-teal-200 transition-colors duration-300">Contact</a>
+        </div>
       </header>
+      <div className="flex ml-auto text-white mt-5 mb-5 px-14">
+        <a href="https://github.com/Teolhyn">
+          <FontAwesomeIcon icon={faSquareGithub} className="size-10" />
+        </a>
+        <a href='https://www.linkedin.com/in/teemuhynna/'>
+          <FontAwesomeIcon icon={faLinkedin} className="size-10 mx-5" />
+        </a>
+        <a href='mailto:teemu.hynna@gmail.com'>
+          <FontAwesomeIcon icon={faEnvelope} className="size-10" />
+        </a>
+      </div>
     </div>
   );
 }
+
+
+
