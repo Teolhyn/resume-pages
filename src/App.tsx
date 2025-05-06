d02a38c2d6735b0c1fd04e2ff0c449a2a161ce08
Teolhyn <84379059+Teolhyn@users.noreply.github.com>
2025-05-06 08:24:39 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ const App = () => {
   return (
     <React.StrictMode>
       <Home />
+      <footer className="p-4 text-center text-sm text-white bg-white dark:bg-black">
+        © {new Date().getFullYear()} Hynnä Consulting Oy.
+      </footer>
     </React.StrictMode>
   );
 };
